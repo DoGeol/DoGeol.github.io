460658ff92bac7afbd847b4b29d47421702e10fc
편도걸 <pdg2491@naver.com>
2024-02-14 21:09:17 +0900

    refactor: next config asset prefix 수정

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@
 const nextConfig = {
   reactStrictMode: false,
   output: 'export',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/dogeol.github.io/' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/dogeol.github.io' : '',
 };
 
 export default nextConfig;
