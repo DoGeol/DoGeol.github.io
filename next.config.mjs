773a635a10ddb488d69d9325e1ac2e2ef6be6c45
편도걸 <pdg2491@naver.com>
2024-02-14 17:53:15 +0900

    refactor: gh-pages 설정, next config 설정 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/dogeol.github.io/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/dogeol.github.io' : '',
+};
 
 export default nextConfig;
