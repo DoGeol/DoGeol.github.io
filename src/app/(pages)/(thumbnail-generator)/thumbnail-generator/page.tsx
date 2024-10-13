04c13a842b6c16f98299b602c0777df551f450cc
편도걸 <pdg2491@naver.com>
2024-10-14 00:04:36 +0900

    feat: 썸네일 제네레이터 페이지 임시 추가

--- src/app/(pages)/(thumbnail-generator)/thumbnail-generator/page.tsx
+++ src/app/(pages)/(thumbnail-generator)/thumbnail-generator/page.tsx
@@ -1,3 +1,5 @@
+import PageClient from '@/app/(pages)/(thumbnail-generator)/thumbnail-generator/page.client'
+
 export default function Page() {
-  return <div>thumbnail-generator</div>
+  return <PageClient />
 }
